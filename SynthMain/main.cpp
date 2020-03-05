66ded24c4ecd3fb7af03976b0526a3764942add6
Garrett <gflynn@usc.edu>
2020-03-04 16:32:32 -0800

    test on L boi's comp

--- SynthMain/main.cpp
+++ SynthMain/main.cpp
@@ -1,6 +1,6 @@
 #include "maximilian.h"
 
-maxiOsc osc1, osc2, osc3, mod1, mod2, mod3;
+maxiOsc osc1, osc2, osc3, osc4, osc5, osc6, mod1, mod2, mod3;
 maxiEnv voladsr, lpfadsr, hpfadsr;
 maxiFilter volfilter, lpf, hpf;
 maxiDistortion dist;
@@ -16,8 +16,8 @@ double myOscOutput = 0.0;
 double myCutoff = 0.2;
 */
 
-double osc, sine, tri, sq = 0.0;
-int osc1Freq = 440;
+double osc, osca, oscb, sine, tri, sq, sine2, tri2, sq2 = 0.0;
+int freq1 = 440;
 int osc2Freq = 370;
 int osc3Freq = 294;
 double sinevol, trivol, sqvol = 1;
@@ -27,14 +27,26 @@ void setup() {//some inits
     
 }
 
+double synth() {
+	sine = osc1.sinewave(freq1);
+	tri = osc2.triangle(freq1);
+	sq = osc3.square(freq1);
+	osca = (sine*sinevol + tri * trivol + sq * sqvol) / (sinevol + trivol + sqvol);
+
+	sine2 = osc4.sinewave(freq1);
+	tri2 = osc5.triangle(freq1);
+	sq2 = osc6.square(freq1);
+	oscb = (sine2*sinevol + tri2 * trivol + sq2 * sqvol) / (sinevol + trivol + sqvol);
+
+	osc = (osca + oscb) / 2;
+
+	return osc;
+}
+
 void play(double *output) {
-    sine = osc1.sinewave(osc1Freq);
-    tri = osc2.triangle(osc2Freq);
-    sq = osc3.square(osc3Freq); 
-    osc = (sine*sinevol+tri*trivol+sq*sqvol)/(sinevol+trivol+sqvol);
     
-    
-    
+	output[0] = synth();
+	output[1] = output[0];
     
     /*
     countIndex = myCounter.phasor(1, 0, 9);
@@ -45,9 +57,6 @@ void play(double *output) {
     
     output[1] = output[0];
     */
-    
-    output[0] = osc;
-    output[1] = output[0];
 }
 
 
