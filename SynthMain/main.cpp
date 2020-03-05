1b73d3c2dd8a8e908799548e8c05e7afe57f4c79
Lorand Cheng <lcheng@macbook.local>
2020-03-04 18:17:03 -0800

    simplified

--- SynthMain/main.cpp
+++ SynthMain/main.cpp
@@ -14,8 +14,8 @@ void setup() {//some inits
 }
 
 double synth(
-	double sinevol, double trivol, double sqvol,
-	double sinevol1, double trivol1, double sqvol1,
+	double sin1vol, double tri1vol, double sq1vol,
+	double sin2vol, double tri2vol, double sq2vol,
 	double mod1f, double mod1v, double mod2f, double mod2v, double mod3f, double mod3v,
 	int mod1ta, int mod2ta, int mod3ta, int mod1tb, int mod2tb, int mod3tb //0 if off 1 if on
 	) {
@@ -24,37 +24,43 @@ double synth(
 	double mod2double = mod2.sinewave(mod2f)*mod2v;
 	double mod3double = mod3.sinewave(mod3f)*mod3v;
 
-	double sine = osc1.sinewave(freq1 + mod1double*mod1ta + mod2.sinewave(mod2f)*mod2ta*mod2v + mod3.sinewave(mod3f)*mod3ta*mod3v);
-	double tri = osc2.triangle(freq1 + mod1.sinewave(mod1f)*mod1ta*mod1v + mod2.sinewave(mod2f)*mod2ta*mod2v + mod3.sinewave(mod3f)*mod3ta*mod3v);
-	double sq = osc3.square(freq1 + mod1.sinewave(mod1f)*mod1ta*mod1v + mod2.sinewave(mod2f)*mod2ta*mod2v + mod3.sinewave(mod3f)*mod3ta*mod3v);
-	double osca = (sine*sinevol + tri * trivol + sq * sqvol) / (sinevol + trivol + sqvol + 0.00000001);
+	double sine = osc1.sinewave(freq1 + mod1double*mod1ta + mod2double*mod2ta + mod3double*mod3ta);
+	double tri = osc2.triangle(freq1 + mod1double*mod1ta + mod2double*mod2ta + mod3double*mod3ta);
+	double sq = osc3.square(freq1 + mod1double*mod1ta + mod2double*mod2ta + mod3double*mod3ta);
+	double osca = (sine*sin1vol + tri * tri1vol + sq * sq1vol) / (sin1vol + tri1vol + sq1vol + 0.00000001);
 
-	double sine2 = osc4.sinewave(freq1 + mod1.sinewave(mod1f)*mod1tb*mod1v + mod2.sinewave(mod2f)*mod2tb*mod2v + mod3.sinewave(mod3f)*mod3tb*mod3v);
-	double tri2 = osc5.triangle(freq1 + mod1.sinewave(mod1f)*mod1tb*mod1v + mod2.sinewave(mod2f)*mod2tb*mod2v + mod3.sinewave(mod3f)*mod3tb*mod3v);
-	double sq2 = osc6.square(freq1 + mod1.sinewave(mod1f)*mod1tb*mod1v + mod2.sinewave(mod2f)*mod2tb*mod2v + mod3.sinewave(mod3f)*mod3tb*mod3v);
-	double oscb = (sine2*sinevol1 + tri2 * trivol1 + sq2 * sqvol1) / (sinevol1 + trivol1 + sqvol1 + 0.00000001);
+	double sine2 = osc4.sinewave(freq1 + mod1double*mod1tb + mod2double*mod2tb + mod3double*mod3tb);
+	double tri2 = osc5.triangle(freq1 + mod1double*mod1tb + mod2double*mod2tb + mod3double*mod3tb);
+	double sq2 = osc6.square(freq1 + mod1double*mod1tb + mod2double*mod2tb + mod3double*mod3tb);
+	double oscb = (sine2*sin2vol + tri2 * tri2vol + sq2 * sq2vol) / (sin2vol + tri2vol + sq2vol + 0.00000001);
 	
 	double osc = (osca + oscb) / 2;
-
-	return osc;
+    
+    return mod1double;
 }
 
 double counter = 0.0;
 
 void play(double *output) {
-    
-	counter+=0.0001;
 
-	double sin1vol = sin(counter);
-    double tri1vol = 0.5;
+	double sin1vol = 1;
+    double tri1vol = 0;
 	double sq1vol = 0;
 	double sin2vol = 0;
 	double tri2vol = 0;
 	double sq2vol = 0;
-	double mod1f, mod2f, mod3f = 440;
-	double mod1v, mod2v, mod3v = 5;
+    double mod1f = 440;
+    double mod2f = 0;
+    double mod3f = 0;
+    double mod1v = 1;
+    double mod2v = 1;
+    double mod3v = 1;
 	int mod1ta = 1;
-	int mod2ta, mod3ta, mod1tb, mod2tb, mod3tb = 0;
+    int mod2ta = 0;
+    int mod3ta = 0;
+    int mod1tb = 0;
+    int mod2tb = 0;
+    int mod3tb = 0;
 
 	output[0] = synth(sin1vol, tri1vol, sq1vol,
 					sin2vol, tri2vol, sq2vol,
