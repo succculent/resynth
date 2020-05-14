cccb72975862e1486ff7d544f0c5052a87ff9705
Lorand Cheng <lfcheng@usc.edu>
2020-05-14 16:55:39 -0700

    filters and envelopes work! I think...

--- SynthMain/main.cpp
+++ SynthMain/main.cpp
@@ -8,15 +8,14 @@ maxiDistortion dist;
 maxiFlanger flang;
 maxiChorus chorus;
 
-int freq = 200;
-
-
-
 void setup() {//some inits
 
 }
 
+
+
 double synth(
+    int trigger, int freq,
     double sin1vol, double tri1vol, double sq1vol,
     double sin2vol, double tri2vol, double sq2vol,
     double mod1f, double mod1v, double mod2f, double mod2v, double mod3f, double mod3v,
@@ -26,6 +25,7 @@ double synth(
     double hiA, double hiD, double hiS, double hiR,
     double volA, double volD, double volS, double volR
     ) {
+
     //carrier frequencies
     double mod1double = mod1.sinewave(mod1f)*mod1v;
     double mod2double = mod2.sinewave(mod2f)*mod2v;
@@ -46,34 +46,50 @@ double synth(
     //Combined oscillator output
     double osc = (osc1.output() + osc2.output()) / 2;
 
+    if (trigger == 1) { //trigger the envelopes
+        lpfadsr.trigger=1;
+        hpfadsr.trigger=1;
+        voladsr.trigger=1;
+    }
+
+    else { //release the envelopes to make it fade out only if it's been triggered
+        lpfadsr.trigger=0;
+        hpfadsr.trigger=0;
+        voladsr.trigger=0;
+    }
+
     //Filter envelopes
     lpfadsr.setAttack(loA);
     lpfadsr.setDecay(loD);
     lpfadsr.setSustain(loS);
     lpfadsr.setRelease(loR);
-    double lobound = lpfadsr.adsr(locutoff);
+    double lobound = locutoff*lpfadsr.adsr(1.,lpfadsr.trigger); //lobound is the envelope shape scaled by the max cutoff frequency
+    double f1osc = lpf.lores(osc, lobound, lores);
+
     hpfadsr.setAttack(hiA);
     hpfadsr.setDecay(hiD);
     hpfadsr.setSustain(hiS);
     hpfadsr.setRelease(hiR);
-    double hibound = hpfadsr.adsr(hicutoff);
-    //Filters
-    double f1osc = lpf.lores(osc, lobound, lores);
-    double f2osc = hpf.hires(osc, hibound, hires);
+    double hibound = freq*5-(freq*5-hicutoff)*hpfadsr.adsr(1.,hpfadsr.trigger); //hibound is the envelope shape scaled by the max cutoff frequency... needs some work
+    double f2osc = hpf.hires(f1osc, hibound, hires);
 
     //Volume envelope
     voladsr.setAttack(volA);
     voladsr.setDecay(volD);
     voladsr.setSustain(volS);
     voladsr.setRelease(volR);
-    
+    double volume = voladsr.adsr(1.,voladsr.trigger);
+
+
     //OUTPUT
-    double out = f1osc; //voladsr.adsr(f2osc);
-    return out;
+    double output = volume*f2osc;
+    return output;
 }
 
+maxiOsc counter;
 void play(double *output) {
-    
+    int freq = 440;
+    int trigger = counter.phasor(1, 1, 9); //basically just a counter 1-10, interates every second and loops
     //volumes of waveforms on 2 oscillators
     double sin1vol = 1;
     double tri1vol = 0;
@@ -99,29 +115,25 @@ void play(double *output) {
     double loA = 0;
     double loD = 1; //needs to be at least 1
     double loS = 1;
-    double loR = 0; //in ms
+    double loR = 1000; //in ms
     double hiA = 0;
     double hiD = 1; //needs to be at least 1
     double hiS = 1;
-    double hiR = 0; //in ms
+    double hiR = 1000; //in ms
     //filter cutoffs and resonances
-    double locutoff = 200;
+    double locutoff = 2000;
     double lores = 10; //awtch out 1-?
-    double hicutoff = 0;
-    double hires = 1;
+    double hicutoff = 440;
+    double hires = 10;
     //volume envelope
     double volA = 0;
     double volD = 1; //needs to be at least 1
     double volS = 1;
-    double volR = 0;
+    double volR = 1000;
 
-    CurrentCount=myCounter.phasor(1, 1, 9); //basically just a counter
-
-    if (CurrentCount==1) myEnvelope.trigger=1; //trigger the envelope
-
-    else myEnvelope.trigger=0;//release the envelope to make it fade out only if it's been triggered
-    
-    output[0] = synth(sin1vol, tri1vol, sq1vol,
+    //left speaker
+    output[0] = synth(trigger, freq,
+                    sin1vol, tri1vol, sq1vol,
                     sin2vol, tri2vol, sq2vol,
                     mod1f, mod1v, mod2f, mod2v, mod3f, mod3v,
                     mod1ta, mod2ta, mod3ta, mod1tb, mod2tb, mod3tb,
@@ -130,6 +142,7 @@ void play(double *output) {
                     hiA, hiD, hiS, hiR,
                     volA, volD, volS, volR
                     );
+    //right speaker
     output[1] = output[0];
 
     /*
