36032c797c7f41027ebf5605c2e53271b26bbbde
Lorand Cheng <lfcheng@usc.edu>
2020-05-27 15:13:11 -0700

    basic synth. Oscillator, envelope, lopass

--- JUCE Synth/Audio Plug-in Synth/Source/SynthVoice.h
+++ JUCE Synth/Audio Plug-in Synth/Source/SynthVoice.h
@@ -23,17 +23,37 @@ public:
     //========================================
     void startNote(int midiNoteNumber, float velocity, SynthesiserSound *sound, int currentPitchWheelPosition)
     {
+        env1.trigger = 1;
+        level = velocity;
         frequency = MidiMessage::getMidiNoteInHertz(midiNoteNumber);
-        std::cout << midiNoteNumber << std::endl;
     }
     //========================================
     void stopNote(float velocity, bool allowTailOff)
     {
-        clearCurrentNote();
+        env1.trigger = 0;
+        allowTailOff = true;
+        //if(velocity == 0)
+            clearCurrentNote();
     }
     //========================================
     void renderNextBlock(AudioBuffer<float> &outputBuffer, int startSample, int numSamples)
     {
+        env1.setAttack(2000);
+        env1.setDecay(500);
+        env1.setSustain(0.8);
+        env1.setRelease(2000);
+        
+        for(int sample = 0; sample < numSamples; sample++)
+        {
+            double wave = osc1.square(frequency);
+            wave = env1.adsr(wave, env1.trigger) * level;
+            wave = filter.lores(wave, 200, 0.1);
+            for(int channel = 0; channel < outputBuffer.getNumChannels(); channel++)
+            {
+                outputBuffer.addSample(channel, startSample, wave);
+            }
+            startSample++;
+        }
         
     }
     //========================================
@@ -51,5 +71,9 @@ public:
 private:
     double level;
     double frequency;
+    
+    maxiOsc osc1;
+    maxiEnv env1;
+    maxiFilter filter;
 };
 
