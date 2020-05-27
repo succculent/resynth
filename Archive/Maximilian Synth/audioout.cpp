aeb86819b00544ac7f534130a82e15884d1bb132
Lorand Cheng <lfcheng@usc.edu>
2020-05-26 18:03:34 -0700

    Cleanup and renaming

---
+++ Archive/Maximilian Synth/audioout.cpp
@@ -0,0 +1,8 @@
+//
+//  audioout.cpp
+//  maximilianTest
+//
+//  Created by Lorand Cheng on 2/19/20.
+//
+
+#include "audioout.hpp"
---
+++ Archive/Maximilian Synth/audioout.hpp
@@ -0,0 +1,13 @@
+//
+//  audioout.hpp
+//  maximilianTest
+//
+//  Created by Lorand Cheng on 2/19/20.
+//
+
+#ifndef audioout_hpp
+#define audioout_hpp
+
+#include <stdio.h>
+
+#endif /* audioout_hpp */
