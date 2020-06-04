d5978d8d2e216f163a7eeb3bd8ca4815c0f11421
Ada Toydemir <Ada Toydemir>
2020-06-04 10:30:53 -0700

    moving to head, small changes on loss function (may not be used

--- optimization/loss.py
+++ optimization/loss.py
@@ -1,14 +1,17 @@
 import scipy
+import numpy
 import math
 
-def loss(x):
+def loss(parameters):
+	#call synth function with parameters and store value into x
+	x = []
 	xFFT = scipy.fft.fft(x)
-	y = [1, 0, 0, 0, 0, 0, 0, 0]
+	y = data = numpy.genfromtxt('targetSound.csv', delimiter=",")
 	yFFT = scipy.fft.fft(y)
 	sum = 0.0
 	sumI = 0.0j
 	divideVal = 0
-	for i in range(xFFT.size):
+	for i in range(math.min(xFFT.size, yFFT.size)):
 		sum += math.pow(xFFT[i].real - yFFT[i].real, 2)
 		sumI += math.pow(xFFT[i].imag - yFFT[i].imag, 2)
 		divideVal += 1
