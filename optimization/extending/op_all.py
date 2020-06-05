04fb5e9406f51bd3e34feed9e9583e1841f1ed1d
Elisabeth Vehling <vehling@usc.edu>
2020-06-05 11:52:28 -0700

    removed initial retrieval of synth autio, fixed name of stereo to mono, added program termination upon filenotfound

--- optimization/extending/op_all.py
+++ optimization/extending/op_all.py
@@ -5,6 +5,7 @@ import numpy as np
 import math
 from os.path import dirname, join as pjoin
 import warnings
+import sys
 
 '''
     Class that handles optimization of the synth parameters from an input sample.
@@ -17,14 +18,14 @@ class Optimize():
         self.__path = filePath
         self.__originalSample = self.getSampleFile()
         self.__sampleLength = len(self.__originalSample)
-        self.__originalSynth = self.getSampleFile() #FIXME change to getSynthOuput()
+        # self.__originalSynth = self.getSampleFile() #FIXME change to getSynthOuput()
         self._FFTSample = self.FFT(self.__originalSample)
-        self._FFTSynth = self.FFT(self.__originalSynth)
+        # self._FFTSynth = self.FFT(self.__originalSynth)
         self.startOptimization()
 
 
     #Converts an array from mono to stereo (actually converts from stereo to mono)
-    def mono2Stereo(self, data):
+    def stereo2Mono(self, data):
         mono = (data[:,0] + data[:,1])/2 
         return mono
 
@@ -35,11 +36,11 @@ class Optimize():
             sampleRate, data = wavfile.read(self.__path)
             data = data.astype(np.float)
             if len(data[0]) == 2:
-                return self.mono2Stereo(data)
+                return self.stereo2Mono(data)
             return data
         except FileNotFoundError:
             print("Error: Input file was not found. Terminating optimization")
-            return 1
+            sys.exit(1)
         # except scipy.io.wavfile.WavFileWarning: #uncomment to silence warnings
             #pass
 
@@ -58,14 +59,9 @@ class Optimize():
     
 
     #TODO Make a call to start the computatioin
-    ''' #ADA# I don't think it is necessary to have the synth output before we
-        start optimization, bc we will likely only be using the synth output 
-        given the parameters from scipy.optimize.minimize - the current
-        parameters may be useful as a starting point, but not the current audio
-    '''
     def startOptimization(self):
         #Check that both the synth and sample input has been convereted 
-        if(all(self._FFTSample) and all(self._FFTSynth)):
+        if(all(self._FFTSample)):
             compute = Compute()
             results = compute.performOptimization()
             self.updateSynthParams(results)
@@ -83,6 +79,7 @@ class Optimize():
     #TODO
     #Update Maxamillian's syth parameters
     def updateSynthParams(self, params):
+        print("Updated parameters:", params) #FIXME testing only
         #convert to C++
         #Extend w c++
         #send the updated synth parameters to maximillian
@@ -98,7 +95,7 @@ class Compute(Optimize):
     def MSELoss(self, parameters):
         #gets synth output based on given parameters and stores the FFT in super().FFTSynth
         super().updateSynthParams(parameters)
-        super().FFTSynth = super().FFT(super().getSynthOutput())
+        FFTSynth = super().FFT(super().getSynthOutput())
         #use super().FFTSynth and super().FFTSample for MSE
         sum = 0.0
         sumI = 0.0j
@@ -115,16 +112,17 @@ class Compute(Optimize):
     def performOptimization(self):
         #self.parameters = minimize(MSELoss, self.parameters, method='L-BFGS-B', jac=False, options={'disp': None})
 
-''' #ADA# Currently some issue passing the func, still haven't figured it out
+        ''' 
+            #ADA# Currently some issue passing the func, still haven't figured it out
 
-        optimizeResult = minimize(self.MSELoss, self._parameters, method='L-BFGS-B', jac=False, options={'disp': None})
-        # self.boundParameters() #FIXME uncomment once exending is done
-        if (optimizeResult.success):
-            print("Optimization successful, saving and loading generated parameters")
-            self.parameters = optimizeResult.x
-        else:
-            print("Optimization terminated: " + optimizeResult.message)
-'''
+                optimizeResult = minimize(self.MSELoss, self._parameters, method='L-BFGS-B', jac=False, options={'disp': None})
+                # self.boundParameters() #FIXME uncomment once exending is done
+                if (optimizeResult.success):
+                    print("Optimization successful, saving and loading generated parameters")
+                    self.parameters = optimizeResult.x
+                else:
+                    print("Optimization terminated: " + optimizeResult.message)
+        '''
         #super().updateSynthParams(self.parameters) #FIXME uncomment once exending is done
         return
 
@@ -138,8 +136,7 @@ class Compute(Optimize):
 def main():
     print("Running optimization...")
     # warnings.filterwarnings("error") #FIXME uncomment with lines 42-43 to silence warnings (i think lol)
-    op = Optimize("./samples/harp.wav") #FIXME change to make path flexible
-    #op = Optimize("./optimize/samples/harp.wav") #FIXME change to make path flexible
+    op = Optimize("./optimize/samples/harp.wav") #FIXME change to make path flexible
     print("Done.")
     return
 
